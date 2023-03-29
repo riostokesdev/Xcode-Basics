d925c69b8538a8a78619431035c15bdf11d73289
Rio Stokes <rio.stokes@gtech.co.uk>
2023-03-29 11:44:37 +0100

    Initial Commit

---
+++ CardApp/CardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardAppApp.swift
+//  CardApp
+//
+//  Created by Rio Stokes on 29/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
